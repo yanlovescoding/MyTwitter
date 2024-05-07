b0f07c238232fbde6b297dbf9afcab96690860d5
Yan Li <oliviaenjoyslife@gmail.com>
2024-05-06 20:00:23 -0700

    create login api

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
-
+        fields = ['username', 'email']
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+
+
+# class LoginSerializer(serializers.ModelSerializer):
+#     username = serializers.CharField()
+#     password = serializers.CharField()
+#
+#     class Meta:
+#         model = User
+#         fields = User
