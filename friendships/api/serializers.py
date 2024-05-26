27236d459d3b34fd0b9122f1d43aaf48a35856ed
Yan Li <oliviaenjoyslife@gmail.com>
2024-05-26 14:56:54 -0700

    Implement follow api

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers,exceptions
 from friendships.models import Friendships
 from accounts.api.serializers import UserSerializerForFriendship
+from django.contrib.auth.models import User
 
 class FollowSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source='from_user')
@@ -17,3 +18,26 @@ class FollowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Friendships
         fields = ('user', 'created_at')
+
+class FriendshipSerializerForCreate(serializers.ModelSerializer):
+    from_user_id = serializers.IntegerField()
+    to_user_id = serializers.IntegerField()
+
+    class Meta:
+        model = Friendships
+        fields = ('from_user_id', 'to_user_id')
+
+    def validate(self, data):
+        if data['from_user_id'] == data['to_user_id']:
+            raise exceptions.ValidationError({
+                'message': 'from_user_id and to_user_id should be different.'
+            })
+        return data
+
+    def create(self, validated_data):
+        from_user_id = validated_data['from_user_id']
+        to_user_id = validated_data['to_user_id']
+        return Friendships.objects.create(
+            from_user_id=from_user_id,
+            to_user_id=to_user_id,
+        )
