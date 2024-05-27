c3831e778b4ce1fff73c9f4b9876cd11bf613604
Yan Li <oliviaenjoyslife@gmail.com>
2024-05-26 17:26:43 -0700

    Implement NewsFeed model

---
+++ newsfeeds/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from newsfeeds.models import NewsFeed
+
+
+@admin.register(NewsFeed)
+class NewsFeedAdmin(admin.ModelAdmin):
+    list_display = ('user', 'tweet', 'created_at')
+    date_hierarchy = 'created_at'
+from django.contrib import admin
+
